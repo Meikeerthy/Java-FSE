816015c6730edbf481e0d79f26a9c9db45c3902c
Meikeerthy <143054017+Meikeerthy@users.noreply.github.com>
2025-05-28 08:31:11 +0530

    Ex3_EvenOrOddChecker.java

--- Java FSE/Module 3/Ex3_EvenOrOddChecker.java
+++ Java FSE/Module 3/Ex3_EvenOrOddChecker.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class EvenOrOddChecker {
+public class Ex3_EvenOrOddChecker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter an integer: ");
