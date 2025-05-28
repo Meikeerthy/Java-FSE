9de7a42a6db31d0f20ab8c679ce48a6bad88ad44
Meikeerthy <143054017+Meikeerthy@users.noreply.github.com>
2025-05-28 08:31:39 +0530

    Ex4_LeapYearChecker.java

--- Java FSE/Module 3/Ex4_LeapYearChecker.java
+++ Java FSE/Module 3/Ex4_LeapYearChecker.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class LeapYearChecker {
+public class Ex4_LeapYearChecker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a year: ");
